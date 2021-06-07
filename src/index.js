c35b2c5a7b8f4fafe48d3169c6ed838ab30d24f4
Chamy <yd95069506@naver.com>
2021-06-07 21:25:44 +0900

    네이버 로그인 추가

--- src/index.js
+++ src/index.js
@@ -9,7 +9,67 @@ app.use(cors());
 
 const {User} = require('./models/User.js');
 
-app.post('/api/user/login', (req, res) => {
+// 네이버 로그인시 사용자 정보를 프론트 단에서는 가져올 수 없어서 서버에서 처리
+app.post('/api/user/naver/login', (req, res) => {
+    const token = req.body.access_token;
+    const header = `Bearer ${token}`;
+
+    const request = require('request');
+    const options = {
+        url: 'https://openapi.naver.com/v1/nid/me',
+        headers: {'Authorization': header}
+    };
+
+    request.get(options, (err, response, body) => {
+        if (!err && response.statusCode == 200) {
+            const profile = JSON.parse(body);
+            
+            User.findOne({email: profile.response.email}, (err, userInfo) => {
+                if (err) {
+                    res.json({success: false});
+                    return;
+                }
+
+                if (!userInfo) {
+                    const newUser = new User({
+                        email: profile.response.email,
+                        image: profile.response.profile_image,
+                        name: profile.response.name,
+                        tokenId: token
+                    });
+                    
+                    newUser.save((err) => {
+                        if (err) {
+                            res.json({success: false});
+                            return;
+                        }
+                    });
+                } else {
+                    User.updateOne({email: profile.response.email}, {tokenId: token}, (err) => {
+                        if (err) {
+                            res.json({success: false});
+                            return;
+                        }
+                    });
+                }
+                res.json({success: true, userInfo: {
+                    name: profile.response.name,
+                    image: profile.response.profile_image
+                }});
+                return;
+            });
+        } else {
+            console.log('Naver login error');
+            if (response !== null) {
+                console.log(`err = ${response.statusCode}`);
+            }
+            res.json({success: false});
+            return;
+        }
+    });
+})
+
+app.post('/api/user/login', async (req, res) => {
     const tokenId = req.headers.authorization;
     User.findOne({email: req.body.email}, (err, userInfo) => {
         if (err) {
